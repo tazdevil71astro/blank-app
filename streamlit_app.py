cfbcb20b3f314aa76c7e6ed2c97e3b7455b80595
tazdevil71astro <tazdevil71.astro.app@gmail.com>
2026-01-26 18:17:59 +0000

    ajout librairies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
+from astropy.time import Time
+import astropy.units as u
+from astropy.coordinates import AltAz, EarthLocation, SkyCoord, get_body, get_sun
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+
+
 st.title("🎈 My new Streamlit app🎈")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."

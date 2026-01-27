6dcd1fe1ab228fe95304e20b132f5759d40e82d8
tazdevil71astro <tazdevil71.astro.app@gmail.com>
2026-01-27 17:15:18 +0000

    suppression bouton ajout jours

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,18 +29,14 @@ input_date = st.date_input("Choisir une nuit d'observation :",
                     today_date.day),
                 format = "DD/MM/YYYY")
 
-select_date = input_date
 
 #Transformation de la date sélectionnée au format str isot pour utilisation dans le graphe
 st.write("Nuit choisie format date :", select_date)
 st.write("Nuit choisie format str isot :", transfo_date_to_isostr(select_date))
 
-#Test bouton +1 mois
-if st.button("+10"):
-    select_date = select_date + datetime.timedelta(days=10)
-    st.write("select_date :", transfo_date_to_isostr(select_date))
 
-Day_obs = transfo_date_to_isostr(select_date)
+
+Day_obs = transfo_date_to_isostr(input_date)
 
 
 
